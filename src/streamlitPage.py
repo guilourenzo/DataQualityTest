f05fbf0f8c2c0d131632060838cdc5370042be35
Guilherme Lourenço <gui.lourenzo@gmail.com>
2023-03-03 10:17:19 -0300

    FILE STRUCTURE

--- src/streamlitPage.py
+++ src/streamlitPage.py
@@ -2,74 +2,79 @@ import streamlit as st
 import pandas as pd
 import pymongo
 import sqlite3
+from configparser import ConfigParser
+from utils.utils import *
 
-# Function to read data from a CSV file
-def read_csv(file_path):
-    return pd.read_csv(file_path)
+CONFIG = ConfigParser()
+CONFIG.read('../config/dq.ini')
+MONGO_SERVER = CONFIG["MONGO_SERVER"]
 
-# Function to read data from a SQL database
-def read_sql(host, port, username, password, database, table):
-    conn = sqlite3.connect(f"sqlite://{host}:{port}/{database}")
-    query = f"SELECT * FROM {table}"
-    return pd.read_sql(query, conn)
-
-# Function to read data from a MongoDB database
-def read_mongo(host, port, database, collection):
-    client = pymongo.MongoClient(host, port)
-    db = client[database]
-    cursor = db[collection].find()
-    return pd.DataFrame(list(cursor))
-
-# Function to register expectations in MongoDB
-def register_expectations_mongo(host, port, database, collection, expectations):
-    client = pymongo.MongoClient(host, port)
-    db = client[database]
-    db[collection].insert_one(expectations)
-    st.success("Expectations registered successfully!")
+HOST = MONGO_SERVER['MONGO_HOST']
+PORT = int(MONGO_SERVER['MONGO_PORT'])
+DB = MONGO_SERVER['MONGO_DB']
+COL = MONGO_SERVER['MONGO_COLLECTIONS']
 
 # UI layout and inputs
 st.title("Data Quality Expectations")
 
+connect, rules, results = st.tabs(['Conexão', 'Regras', 'Resultados'])
+
+with connect:
 # Select data source type
-data_source = st.selectbox("Select data source type", options=["CSV", "SQL", "MongoDB"])
+    data_source = st.selectbox("Select data source type", options=["CSV", "SQL", "MongoDB"])
 
-# Read data
-if data_source == "CSV":
-    file_path = st.file_uploader("Upload CSV file")
-    if file_path is not None:
-        data = read_csv(file_path)
-else:
-    host = st.text_input("Host")
-    port = st.number_input("Port", value=0, step=1)
-    if data_source == "SQL":
-        username = st.text_input("Username")
-        password = st.text_input("Password", type="password")
-        database = st.text_input("Database")
-        table = st.text_input("Table")
-        if host and port and username and password and database and table:
-            data = read_sql(host, port, username, password, database, table)
-    elif data_source == "MongoDB":
-        database = st.text_input("Database")
-        collection = st.text_input("Collection")
-        if host and port and database and collection:
-            data = read_mongo(host, port, database, collection)
+    # Read data
+    if data_source == "CSV":
+        file_path = st.file_uploader("Upload CSV file")
+        if file_path is not None:
+            csv = read_csv(file_path)
+            data = csv[0]
+            connection = csv[1]
+    else:
+        host = st.text_input("Host")
+        port = st.number_input("Port", value=0, step=1)
+        if data_source == "SQL":
+            username = st.text_input("Username")
+            password = st.text_input("Password", type="password")
+            database = st.text_input("Database")
+            table = st.text_input("Table")
+            if host and port and username and password and database and table:
+                sql = read_sql(host, port, username, password, database, table)
+                data = sql[0]
+                connection = sql[1]
 
-# Select column to apply expectations
-if "data" in locals():
-    col_options = data.columns.tolist()
-    selected_col = st.selectbox("Select column to apply expectations", options=col_options)
+        elif data_source == "MongoDB":
+            database = st.text_input("Database")
+            collection = st.text_input("Collection")
+            if host and port and database and collection:
+                mongo = read_mongo(host, port, database, collection) 
+                data = mongo[0]
+                connection = mongo[1]
 
-    # Select expectations to apply
-    st.write("Select expectations to apply to the column:")
-    expectation_options = ["expect_column_to_exist", "expect_column_values_to_not_be_null", "expect_column_values_to_be_in_set"]
-    selected_expectations = st.multiselect("Expectations", options=expectation_options)
+with rules:
+    # Select column to apply expectations
+    if "data" in locals():
+        col_options = data.columns.tolist()
+        selected_col = st.selectbox("Select column to apply expectations", options=col_options)
 
-    # Create expectations dictionary
-    expectations = {
-        "column_name": selected_col,
-        "expectations": selected_expectations
-    }
+        # Select expectations to apply
+        st.write("Select expectations to apply to the column:")
+        expectation_options = ["expect_column_to_exist", "expect_column_values_to_not_be_null", "expect_column_values_to_be_in_set"]
+        selected_expectations = st.multiselect("Expectations", options=expectation_options)
 
-    # Register expectations
-    if st.button("Register expectations"):
-        register_expectations_mongo(host, port, database, collection, expectations)
+        # Create expectations dictionary
+        expectations = {
+            "file_path": connection,
+            "column_name": selected_col,
+            "expectations": selected_expectations
+        }
+
+        # Register expectations
+        if st.button("Register expectations"):
+            st.header(f'{HOST} | {PORT} | {DB} | {COL}')
+            
+            register_expectations_mongo(HOST, PORT, DB, COL, expectations)
+            st.success("Expectations registered successfully!")
+
+with results:
+    st.header('EM DESENVOLVIMENTO')
---
+++ src/utils/utils.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import pymongo
+import sqlite3
+
+# Function to read data from a CSV file
+def read_csv(file_path):
+    return pd.read_csv(file_path), str(f'{file_path}')
+
+# Function to read data from a SQL database
+def read_sql(host, port, username, password, database, table):
+    conn = sqlite3.connect(f"sqlite://{host}:{port}/{database}")
+    query = f"SELECT * FROM {table}"
+    return pd.read_sql(query, conn), str(f'sqlite3.connect(f"sqlite://{host}:{port}/{database}")')
+
+# Function to read data from a MongoDB database
+def read_mongo(host, port, database, collection):
+    client = pymongo.MongoClient(host, port)
+    db = client[database]
+    cursor = db[collection].find()
+    return pd.DataFrame(list(cursor)), str(f'pymongo.MongoClient({host}, {port})')
+
+# Function to register expectations in MongoDB
+def register_expectations_mongo(host, port, database, collection, expectations):
+    print(host)
+    client = pymongo.MongoClient(host, port)
+    db = client[database]
+    db[collection].insert_one(expectations)
+    
